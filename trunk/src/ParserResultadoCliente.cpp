3f6eccd0f66ddca591127c4403f407cdce1261e2
tatoqac@gmail.com <tatoqac@gmail.com@53b52b14-cf4e-0b1c-12fa-8689e3808acb>
2010-09-15 03:03:10 +0000

    puse un delete y saque unos comentarios

--- trunk/src/ParserResultadoCliente.cpp
+++ trunk/src/ParserResultadoCliente.cpp
@@ -1,10 +1,3 @@
-/*
- * ParserResultadoCliente.cpp
- *
- *  Created on: 30/08/2010
- *      Author: gaston
- */
-
 #include "ParserResultadoCliente.h"
 
 ParserResultadoCliente::ParserResultadoCliente() {
