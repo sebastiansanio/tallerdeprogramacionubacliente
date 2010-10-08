6fe2f466715e51e33532cabb9353c3d3f9e1c467
klacalabresi@gmail.com <klacalabresi@gmail.com@53b52b14-cf4e-0b1c-12fa-8689e3808acb>
2010-10-08 20:10:55 +0000

    Estuve ordenando codigo, saqué cosas del tp anterior, falta ver algunas cosas. Ahora el juego maneja el cliente, la pantalla, el parsercliente y el parserresultado por separado

--- trunk/src/Cliente.cpp
+++ trunk/src/Cliente.cpp
@@ -1,9 +1,6 @@
 #include "Cliente.h"
 
 Cliente::Cliente() {
-	this->parserResultado=new ParserResultadoCliente();
-	this->parser= new ParserCliente();
-
 	//creamos el socket
 	descriptorSocket=socket(AF_INET,SOCK_STREAM,0);
 	if(descriptorSocket==-1){
@@ -58,171 +55,71 @@ void Cliente::enviar(char* data){
 	delete []data2;
 }
 
-void Cliente::interactuarConUsuarioYservidor(){
+//void Cliente::recibir(){
+//	char* data=new char[MAXBYTESRECIBIDOS];
+//	memset((void*)data,'\0',MAXBYTESRECIBIDOS);
+//	bool seguir=true;
+//	ofstream* archivoResultado = new ofstream("recibido", ios::out);
+//	socklen_t leng=sizeof(char[MAXBYTESRECIBIDOS]);
+//	ssize_t valorRecive;
+//	while(seguir){
+//		valorRecive=recv(this->descriptorSocket,data,leng,0);
+//		if(valorRecive==0){
+//       		cout<<"Se desconecto el servidor.."<<endl;
+//       		cout<<"Se cerrará la aplicación"<<endl;
+//       		sleep(2);
+//       		exit(0);
+//		}
+//		if(valorRecive==-1){
+//			cout<<"Mal recibido"<<endl;
+//		}else{
+//			//corroboro que los ultimos tres formen eof
+//			if((data[valorRecive-1]=='f')and(data[valorRecive-2]=='o')and(data[valorRecive-3]=='e')){
+//				seguir=false;
+//				ostringstream sstream;
+//				sstream << data;
+//				string lineaActual = sstream.str();
+//				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
+////				Para sacar el eof del archivo
+//				string::iterator it=lineaActual.end();
+//				it--;
+//				it=lineaActual.erase(it);
+//				it--;
+//				it=lineaActual.erase(it);
+//				it--;
+//				it=lineaActual.erase(it);
+//				*archivoResultado<<lineaActual;
+//			}else{
+//				ostringstream sstream;
+//				sstream << data;
+//				string lineaActual = sstream.str();
+//				*archivoResultado<<lineaActual;
+//				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
+//				delete data;
+//				data=new char[MAXBYTESRECIBIDOS];
+//				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
+//			}
+//		}
+//	}
+//	delete data;
+//	archivoResultado->close();
+//	delete archivoResultado;
+//	string recibido;
+//	string *recibidoAux=new string;
+//	ifstream* archivo=new ifstream("recibido");
+//	while(!archivo->eof()){
+//		std::getline(*archivo,*recibidoAux);
+//		recibido+=*recibidoAux;
+//		recibido+="\n";
+//	}
+//	char* dataAux=new char[recibido.size()];
+//	memset(dataAux,'\0',recibido.size());
+//	for(int i=0;i<recibido.size();i++) dataAux[i]=recibido[i];
+//	this->parserResultado->DecodificaResultado(dataAux);
+//	delete dataAux;
+//	archivo->close();
+//}
 
-	system("clear");
-
-	string idOperacion;
-	list<string>* operandos=new list<string>();
-
-	//Selección de Operacion
-	cout<<"Ingrese el id de la operacion:"<<endl;
-	cout<<"		S ----> Suma"<<endl;
-	cout<<"		R ----> Resta"<<endl;
-	cout<<"		M ----> Multiplicación"<<endl;
-	cout<<"		D ----> División"<<endl;
-	cin>>idOperacion;
-	//Para pasar a mayuscula
-	idOperacion=toupper(idOperacion[0]);
-
-	while((idOperacion!="S")and(idOperacion!="R")and(idOperacion!="M")and(idOperacion!="D")){
-		cout<<"Ingrese una operacion valida"<<endl;
-		cin>>idOperacion;
-		idOperacion=toupper(idOperacion[0]);
-	}
-
-	string nombreOperando;
-	string valorOperando;
-	string respuesta;
-
-	//Ingreso de operandos
-	switch(idOperacion[0]){
-		case ('S'):nombreOperando="sum";respuesta="S";break;
-		case ('R'):nombreOperando="res";respuesta="S";break;
-		case ('M'):nombreOperando="mul";respuesta="S";break;
-		case ('D'):respuesta="N";
-					cout<<"Ingrese dividendo:"<<endl;
-					cin>>valorOperando;
-					operandos->push_back("dividendo");
-					operandos->push_back(valorOperando);
-					cout<<"Ingrese divisor:"<<endl;
-					cin>>valorOperando;
-					operandos->push_back("divisor");
-					operandos->push_back(valorOperando);
-					break;
-	}
-
-	//Lectura de operandos en caso que no se haya elegido una division
-	while ((respuesta=="S")){
-		cout<<"Ingresar operando"<<endl;
-		cin>>valorOperando;
-		operandos->push_back(nombreOperando);
-		operandos->push_back(valorOperando);
-		cout<<"Desea ingresar otro operando? S/N"<<endl;
-		cin>>respuesta;
-		respuesta=toupper(respuesta[0]);
-		while((respuesta!="S")and(respuesta!="N")){
-			cout<<"Desea ingresar otro operando? S/N"<<endl;
-			cin>>respuesta;
-			respuesta=toupper(respuesta[0]);
-		}
-	}
-	//Generacion y envio de xml
-	char* xml=this->parser->getXmlDeOperacion(idOperacion,operandos);
-	enviar(xml);
-	system("clear");
-	cout<<"Mensaje enviado al Servidor"<<endl;
-	cout<<endl;
-	cout<<"Esperando Respuesta de la operación"<<endl;
-	cout<<endl;
-	this->recibir();
-	sleep(2);
-}
-
-void Cliente::enviarArchivoOperaciones(string nombreArchivo){
-	const char* nombreArchivoString = nombreArchivo.c_str();
-	ParserCliente* parserArchivo= new ParserCliente(nombreArchivoString);
-	int numeroOperacion=1;
-	if((parserArchivo->comprobarSintaxis())==false){
-		if(parserArchivo->huboErrorAlAbrirArchivo()){
-			cout<<"Hubo un error al abrir el archivo, compuebe el nombre o dirección del mismo"<<endl;
-			sleep(2);
-		}else{
-			cout<<"Error de sintaxis, ver archivo \"errores.err\""<<endl;
-			sleep(2);}
-	} else {
-		char* xml = parserArchivo->getSiguienteOperacion();
-		while(strcmp(xml,"")!=0){
-			enviar(xml);
-			system("clear");
-			cout<<"Mensaje enviado al Servidor"<<endl;
-			cout<<endl;
-			cout<<"Esperando Respuesta de la operación nº: "<<numeroOperacion<<endl;
-			cout<<endl;
-			this->recibir();
-			sleep(2);
-			xml = parserArchivo->getSiguienteOperacion();
-			numeroOperacion++;
-		}
-	}
-	delete parserArchivo;
-}
-
-void Cliente::recibir(){
-	char* data=new char[MAXBYTESRECIBIDOS];
-	memset((void*)data,'\0',MAXBYTESRECIBIDOS);
-	bool seguir=true;
-	ofstream* archivoResultado = new ofstream("recibido", ios::out);
-	socklen_t leng=sizeof(char[MAXBYTESRECIBIDOS]);
-	ssize_t valorRecive;
-	while(seguir){
-		valorRecive=recv(this->descriptorSocket,data,leng,0);
-		if(valorRecive==0){
-       		cout<<"Se desconecto el servidor.."<<endl;
-       		cout<<"Se cerrará la aplicación"<<endl;
-       		sleep(2);
-       		exit(0);
-		}
-		if(valorRecive==-1){
-			cout<<"Mal recibido"<<endl;
-		}else{
-			//corroboro que los ultimos tres formen eof
-			if((data[valorRecive-1]=='f')and(data[valorRecive-2]=='o')and(data[valorRecive-3]=='e')){
-				seguir=false;
-				ostringstream sstream;
-				sstream << data;
-				string lineaActual = sstream.str();
-				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
-//				Para sacar el eof del archivo
-				string::iterator it=lineaActual.end();
-				it--;
-				it=lineaActual.erase(it);
-				it--;
-				it=lineaActual.erase(it);
-				it--;
-				it=lineaActual.erase(it);
-				*archivoResultado<<lineaActual;
-			}else{
-				ostringstream sstream;
-				sstream << data;
-				string lineaActual = sstream.str();
-				*archivoResultado<<lineaActual;
-				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
-				delete data;
-				data=new char[MAXBYTESRECIBIDOS];
-				memset((void*)data,'\0',MAXBYTESRECIBIDOS);
-			}
-		}
-	}
-	delete data;
-	archivoResultado->close();
-	delete archivoResultado;
-	string recibido;
-	string *recibidoAux=new string;
-	ifstream* archivo=new ifstream("recibido");
-	while(!archivo->eof()){
-		std::getline(*archivo,*recibidoAux);
-		recibido+=*recibidoAux;
-		recibido+="\n";
-	}
-	char* dataAux=new char[recibido.size()];
-	memset(dataAux,'\0',recibido.size());
-	for(int i=0;i<recibido.size();i++) dataAux[i]=recibido[i];
-	this->parserResultado->DecodificaResultado(dataAux);
-	delete dataAux;
-	archivo->close();
-
-}
 char* Cliente::recibirRespuesta(){
 	char* data=new char[MAXBYTESRECIBIDOS];
 	memset((void*)data,'\0',MAXBYTESRECIBIDOS);
@@ -295,7 +192,6 @@ string Cliente::recibirArchivo(string path){
 	socklen_t leng=sizeof(char[MAXBYTESRECIBIDOS]);
 	ssize_t valorRecive;
 	while(seguir){
-		cout<<"Se esta recibiendo el archivo"<<endl;
 		valorRecive=recv(this->descriptorSocket,data,leng,0);
 		if(valorRecive==0){
        		cout<<"Se desconecto el servidor.."<<endl;
@@ -329,6 +225,4 @@ string Cliente::recibirArchivo(string path){
 	return path;
 }
 Cliente::~Cliente() {
-	delete this->parser;
-	delete this->parserResultado;
 }
--- trunk/src/Principal.cpp
+++ trunk/src/Principal.cpp
@@ -1,105 +1,25 @@
 using namespace std;
-#define PATH "/home/gaston/workspace/TpTallerDeProgramacionICliente/config.ini"
 #include <string>
 #include <stdlib.h>
 #include <ctype.h>
-#include "Cliente.h"
 #include <unistd.h>
-#include "Pantalla.h"
+#include "Juego.h"
 #include "math.h"
-#include "BitMap.h"
-#include "ParserResultadoCliente.h"
 
 int main(){
-	Cliente* cliente= new Cliente();
-//	Cliente * cliente=new Cliente(PATH); IRIA ESTEEEEEEE
-	cliente->conectar();
-	bool seguir=true;
-	//empieza la interacción usuario - cliente - servidor
-	while(seguir){
-		system("clear");
-		cout<<"Elija la opción deseada:"<<endl;
-		cout<<"1 - Ingresar operacion"<<endl;
-		cout<<"2 - Levantar archivo de operaciones"<<endl;
-		cout<<"3 - Pruebas de pantalla"<<endl;
-		cout<<"0 - Salir"<<endl;
-		string respuesta;
-		cin>>respuesta;
-		while((respuesta!="1")and(respuesta!="2")and(respuesta!="0")and(respuesta!="3")){
-				cout<<"Ingrese una opción valida"<<endl;
-				cin>>respuesta;
-		}
-		switch(respuesta[0]){
-			case '1':{cliente->interactuarConUsuarioYservidor();break;}
-			case '2':{
-				string nombreArchivo;
-				cout<<"Ingrese el nombre del archivo"<<endl;
-				cin>>nombreArchivo;
-				cliente->enviarArchivoOperaciones(nombreArchivo);
+	Juego* juego = new Juego();
+	juego->pedirEscenario();
+	juego->dibujarEscenario();
+	//Empiezo a mostrar
+	SDL_Event evento;
+	bool terminar=false;
+	while(!terminar){
+		while(SDL_PollEvent(&evento)) {
+			if((evento.type == SDL_QUIT)){
+				terminar=true;
 			}
-				break;
-			case '3':{
-					int alto=700;
-					int ancho=1358;
-					string ruta="pantalla.bmp";
-					ParserCliente *parser=new ParserCliente();
-					ParserResultadoCliente * parserResultado=new ParserResultadoCliente();
-
-					//Pido el escenario
-					string idOperacion="E";
-					list<string>* operandos=new list<string>();
-					char* xml=parser->getXmlDeOperacion(idOperacion,operandos);
-					cliente->enviar(xml);
-					BitMap* escenario=new BitMap(cliente->recibirArchivo(ruta));
-					delete operandos;
-/*					//Pido los jugadores
-					string idOperacion="J";
-					list<string>* operandos=new list<string>();
-					char* xml=parser->getXmlDeOperacion(idOperacion,operandos);
-					cliente->enviar(xml);
-					char * respuesta=cliente->recibirRespuesta();
-					list<string> * jugadores=parserResultado->getJugadores(respuesta);
-
-					//Pido cartas en juego
-					string idOperacion="C";
-					list<string>* operandos=new list<string>();
-					char* xml=parser->getXmlDeOperacion(idOperacion,operandos);
-					cliente->enviar(xml);
-					char * respuesta=cliente->recibirRespuesta();
-					list<string> * cartas=parserResultado->getCartas(respuesta);
-
-					//Pido el poso acumulado
-					string idOperacion="P";
-					list<string>* operandos=new list<string>();
-					char* xml=parser->getXmlDeOperacion(idOperacion,operandos);
-					cliente->enviar(xml);
-					char * respuesta=cliente->recibirRespuesta();
-					string posoAcumulado=parserResultado->getPoso(respuesta);*/
-
-					//Empiezo a mostrar
-					Pantalla* pantalla=new Pantalla(alto,ancho);
-					if(escenario->esUnaImagenCorrecta()){
-						escenario->resizeTo(alto,ancho);
-						pantalla->dibujarBitMapDesdePos((*escenario),0,0);
-					}else{
-						cout<<"No es una imagen corecta"<<endl;
-					}
-					SDL_Event evento;
-					bool terminar=false;
-					while(!terminar){
-						while(SDL_PollEvent(&evento)) {
-							if((evento.type == SDL_QUIT)){
-								terminar=true;
-								break;
-							}
-						}
-					}
-					delete pantalla;
-					break;
-			}
-			case '0': seguir=false; break;
 		}
 	}
-	delete cliente;
+	delete juego;
 	return 0;
 }
